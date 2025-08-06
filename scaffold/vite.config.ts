22e6cb39101d84c1c9d3bbaa3d437e4769696e3e
IzuanJamal <muhamadizuanjamal@gmail.com>
2025-08-06 13:55:48 +0800

    Update vite.config.ts

--- scaffold/vite.config.ts
+++ scaffold/vite.config.ts
@@ -1,17 +1,10 @@
-import { defineConfig } from "vite";
-import dyadComponentTagger from "@dyad-sh/react-vite-component-tagger";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite'
+import path from 'path'
 
-export default defineConfig(() => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [dyadComponentTagger(), react()],
+export default defineConfig({
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-}));
+})
